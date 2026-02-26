f6902f430854a4fa6c000670689a494253f0b706
kevinhayrle <kevinantonyofficial@gmail.com>
2026-02-26 13:43:12 +0530

    c

--- js/preloader.js
+++ js/preloader.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const loader = document.getElementById("preloader");
+  if (!loader) return;
 
   setTimeout(() => {
     loader.style.opacity = "0";
+
     setTimeout(() => {
       loader.style.display = "none";
     }, 600);
-  }, 800);
+  }, 1200);
 });
